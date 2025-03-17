2655e9226ddae08ab478e5a79728d40f12966bbc
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2025-03-17 21:42:41 +0100

    Move folder

--- tests/Integration.Tests/PaperController/PaperControllerUnauthenticatedIntegrationTest.cs
+++ tests/Integration.Tests/PaperController/PaperControllerUnauthenticatedIntegrationTest.cs
@@ -7,6 +7,8 @@ using Data.Entities;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Moq;
 
+namespace Integration.Tests.PaperController;
+
 public class PaperControllerUnauthenticatedIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
