48a3c186aa3b33a301607f92901d27bd9a7b5a88
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2025-06-23 22:28:32 +0200

    Add TestDataOptions as property for test

--- tests/Integration.Tests/TestData/BaseIntegrationTest.cs
+++ tests/Integration.Tests/TestData/BaseIntegrationTest.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Net.Http;
+using Integration.Tests.TestData.Auth;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Persistence;
 
 namespace Integration.Tests.TestData;
 
-public abstract class BaseIntegrationTest(IntegrationTestSessionFactory sessionFactory)
+public abstract class BaseIntegrationTest(
+    IntegrationTestSessionFactory sessionFactory)
 {
-    protected readonly AppDbContext DbContext =
-        sessionFactory.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
-
-    public readonly HttpClient HttpClientAuthenticated = sessionFactory.CreateClient(
+    protected readonly AppDbContext DbContext = sessionFactory.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+    protected readonly HttpClient HttpClientAuthenticated = sessionFactory.CreateClient(
         new WebApplicationFactoryClientOptions
         {
             BaseAddress = new Uri("http://localhost:5070/")
         });
+    protected readonly TestDataOptions TestData = sessionFactory.Services.CreateScope().ServiceProvider.GetRequiredService<IOptions<TestDataOptions>>().Value;
 }
