7dbdeb3d0ac6143216db4ff82a5ce6118b790610
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2025-06-23 22:28:43 +0200

    Add integration tests

---
+++ tests/Integration.Tests/PaperController/PortfolioActions/GetPortfolioTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Api.Controllers;
+using Data.Entities;
+using Integration.Tests.TestData;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration.Tests.Paper.PortfolioActions;
+
+[Collection("TestContainer Db")]
+[TestSubject(typeof(PaperController))]
+public class GetPortfolioTests(IntegrationTestSessionFactory factory) : BaseIntegrationTest(factory)
+{
+    private const decimal DepositAmount = 100m;
+
+    [Trait("Category", "Integration test")]
+    [Fact(DisplayName = "Should successfully return portfolio")]
+    public async Task GetPortfolio_ShouldReturnPortfolio()
+    { 
+        var response = await HttpClientAuthenticated.GetAsync($"api/paper/portfolio/{TestData.TestAuthUserA.PortfolioId}");
+        
+        Assert.NotNull(response);
+        Assert.True(response.IsSuccessStatusCode);
+        var portfolio = await response
+            .Content
+            .ReadFromJsonAsync<Portfolio>();
+        
+        Assert.NotNull(portfolio);
+        
+        var persistedPortfolio = await DbContext
+            .Portfolios
+            .Include(p => p.Holdings)
+            .FirstOrDefaultAsync(p => p.Id == new Guid(TestData.TestAuthUserA.PortfolioId));
+        
+        Assert.NotNull(persistedPortfolio);
+
+        Assert.Equivalent(portfolio, persistedPortfolio);
+    }
+
+    [Trait("Category", "Integration test")]
+    [Fact(DisplayName = "Should fail to get not existing portfolio")]
+    public async Task? GetPortfolio_WhenPortfolioDoesNotExist_ShouldReturnError()
+    {
+        var response = await HttpClientAuthenticated.GetAsync($"api/paper/portfolio/{Guid.Empty}");
+        
+        Assert.NotNull(response);
+        Assert.Equal(System.Net.HttpStatusCode.Forbidden, response.StatusCode);
+    }
+}
