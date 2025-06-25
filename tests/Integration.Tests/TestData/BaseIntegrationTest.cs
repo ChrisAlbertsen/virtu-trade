22e8af5dbb087d3f1d3539d3ab5c1145ffad6ccb
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2025-06-25 12:26:09 +0200

    Ensure test container is used

--- tests/Integration.Tests/TestData/BaseIntegrationTest.cs
+++ tests/Integration.Tests/TestData/BaseIntegrationTest.cs
@@ -8,14 +8,28 @@ using Persistence;
 
 namespace Integration.Tests.TestData;
 
-public abstract class BaseIntegrationTest(
-    IntegrationTestSessionFactory sessionFactory)
+public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestSessionFactory>, IDisposable
 {
-    protected readonly AppDbContext DbContext = sessionFactory.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
-    protected readonly HttpClient HttpClientAuthenticated = sessionFactory.CreateClient(
-        new WebApplicationFactoryClientOptions
-        {
-            BaseAddress = new Uri("http://localhost:5070/")
-        });
-    protected readonly TestDataOptions TestData = sessionFactory.Services.CreateScope().ServiceProvider.GetRequiredService<IOptions<TestDataOptions>>().Value;
+    private readonly IServiceScope _scope;
+    protected readonly AppDbContext DbContext;
+    protected readonly HttpClient HttpClientAuthenticated;
+    protected readonly TestDataOptions TestData;
+
+    protected BaseIntegrationTest(IntegrationTestSessionFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+        DbContext = _scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        HttpClientAuthenticated = factory.CreateClient(
+            new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost:5070/")
+            });
+        TestData = _scope.ServiceProvider.GetRequiredService<IOptions<TestDataOptions>>().Value;
+    }
+
+    public void Dispose()
+    {
+        _scope.Dispose();
+    }
+    
 }
